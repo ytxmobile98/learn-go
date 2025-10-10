c293d349b530bb6358eaea5275221e2cb88d496b
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2025-10-10 14:50:22 +0800

    feat: unmarshal yaml with anchors

--- examples/yaml-custom-unmarshal/main.go
+++ examples/yaml-custom-unmarshal/main.go
@@ -15,29 +15,62 @@ func getDataFile() string {
 	return path.Join(dir, "data.yaml")
 }
 
-func unmarshalYaml(filename string) (any, error) {
+func unmarshalYaml[T any](filename string) (*T, error) {
 	bytes, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	var a yaml.Unmarshaler = &CustomData{}
-	err = yaml.Unmarshal(bytes, a)
-	return a, err
+	var a T
+	err = yaml.Unmarshal(bytes, &a)
+	return &a, err
 }
 
-type CustomData map[any]any
+type Data struct {
+	Items  Anchors `yaml:"items"`
+	Groups Refs    `yaml:"groups"`
+}
 
-func (c *CustomData) UnmarshalYAML(value *yaml.Node) error {
-	fmt.Printf("Value: %+v\n", value)
+type Person struct {
+	Name string `yaml:"name"`
+	Id   int    `yaml:"id"`
+}
+type Anchors map[string]*Person
+type Refs map[string][]*Person
+
+func (a *Anchors) UnmarshalYAML(value *yaml.Node) error {
+	*a = make(Anchors)
+
+	for _, c := range value.Content {
+		anchorName := c.Anchor
+
+		var person Person
+		err := c.Decode(&person)
+		if err != nil {
+			return err
+		}
+
+		(*a)[anchorName] = &person
+	}
 	return nil
 }
 
 func main() {
 	yamlFile := getDataFile()
-	data, err := unmarshalYaml(yamlFile)
+
+	var data *Data
+	data, err := unmarshalYaml[Data](yamlFile)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(data)
+
+	for anchor, item := range data.Items {
+		fmt.Printf("%s: (%p) %+v\n", anchor, item, *item)
+	}
+	for group, items := range data.Groups {
+		fmt.Println(group, items)
+		for _, item := range items {
+			fmt.Println("  ", item)
+		}
+	}
 }
