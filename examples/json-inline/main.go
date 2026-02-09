a1ff856d58f5b69aa7d70ff73a3af8aa971f028a
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2026-02-09 11:46:18 +0800

    feat: more advanced inline example

--- examples/json-inline/main.go
+++ examples/json-inline/main.go
@@ -5,25 +5,53 @@ import (
 	"fmt"
 )
 
-type Foo struct {
-	A `json:",inline"`
+type Base struct {
+	Type        string `json:"type"`
+	*Coordinate `json:",inline"`
+	*Name       `json:",inline"`
 }
 
-type A struct {
-	X int `json:"X"`
-	Y int `json:"Y"`
+type Coordinate struct {
+	Type string `json:"type"`
+
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type Name struct {
+	Type string `json:"type"`
+
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 }
 
 func main() {
-	data := []byte(`{
-		"X": 1,
-		"Y": 2
+	data1 := []byte(`{
+		"type": "coordinate",
+		"x": 1,
+		"y": 2
+	}`)
+	data2 := []byte(`{
+		"type": "name",
+		"firstName": "John",
+		"lastName": "Doe"
 	}`)
 
-	var foo Foo
-	_ = json.Unmarshal(data, &foo)
+	for _, data := range [][]byte{data1, data2} {
+		var base Base
+		err := json.Unmarshal(data, &base)
+		fmt.Printf("Base: %+v %v\n", base, err)
 
-	fmt.Printf("%+v\n", foo.A)
-	fmt.Printf("%+v\n", foo.X)
-	fmt.Printf("%+v\n", foo.Y)
+		switch true {
+		case base.Coordinate != nil:
+			var coordinate Coordinate
+			err := json.Unmarshal(data, &coordinate)
+			fmt.Printf("Coordinate: %+v %v\n", coordinate, err)
+		case base.Name != nil:
+			var name Name
+			err := json.Unmarshal(data, &name)
+			fmt.Printf("Name: %+v %v\n", name, err)
+		}
+	}
+
 }
